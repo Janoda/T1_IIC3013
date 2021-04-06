b6cbba45bb526077e32313f238dda4c5043c8c9e
Joaquin <jiaraya4@uc.cl>
2021-04-06 01:09:40 -0400

    home and seasons

---
+++ src/components/home.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import Container from 'react-bootstrap/Container';
+import { Jumbotron, Table } from 'react-bootstrap';
+import {
+  Link,
+} from "react-router-dom";
+function Home() {
+
+    const dataB = [1,2,3,4,5];
+      
+    
+    return (
+
+      <Container className="p-3">
+        
+        <Jumbotron>
+        <h1 className="header">
+          Series
+        </h1>
+        
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Serie</th>
+              <th>Temporada</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {dataB.map(el =>(
+              <tr>
+                <td>Breaking Bad</td>
+                <td><Link to={"/series/Breaking Bad/" + el}>Temporada {el}</Link></td>
+              </tr>
+            ))}
+            {dataB.map(el =>(
+              <tr>
+                <td>Better Call Saul</td>
+                <td><Link to={"/episodes/?series=better+call+saul&season=" + el}>Temporada {el}</Link></td>
+              </tr>
+            ))}
+           
+          </tbody>
+        </Table>
+  
+        </Jumbotron>
+   
+        
+      </Container>
+
+    );
+  }
+
+  export default Home;
---
+++ src/components/season.jsx
@@ -0,0 +1,49 @@
+import {React, useEffect, useState} from "react"
+import { Container, Jumbotron } from "react-bootstrap";
+import {
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import axios from 'axios';
+
+
+function Season() {
+  const match  = useRouteMatch();
+  const [episodes, setEpisodes] = useState([])
+  useEffect(() => {
+    // GET request using axios inside useEffect React hook
+    axios.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul')
+        .then(response => {
+          const data = response.data
+          setEpisodes(data)
+          console.log(data)
+        });
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+}, [setEpisodes]);
+  let { seriesname, seasonid } = useParams();
+
+    return (
+      <Container>
+        <Jumbotron>
+          <h1 className = "header"> {seriesname} temporada {seasonid} </h1>
+        <ul className = "list-group">
+          {episodes.map(el => {
+            if (el.season === seasonid){
+              return(
+              <li className = "list-group-item"><Link to={`${match.url}/${el.episode_id}`}>{el.title}</Link> </li>
+              )
+            }
+          })}
+        </ul>
+
+        </Jumbotron>
+      </Container>
+        
+
+      
+      );
+
+  }
+export default Season
